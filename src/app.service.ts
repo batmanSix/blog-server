14084853b7f2fe981397d31b21199bc1b2c8c441
kangbo <841331654@qq.com>
2022-08-15 15:25:24 +0800

    格式化代码测试

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!213我的第一个nest服务';
   }
 }
