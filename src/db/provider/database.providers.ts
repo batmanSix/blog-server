64aaf17b2235dd811fdc93e024d1f3e9cf450b2b
kangbo <841331654@qq.com>
2022-08-16 10:12:33 +0800

    提交修改

---
+++ src/db/provider/database.providers.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+
+export const databaseProviders = [
+// {
+//   provide: 'DATA_SOURCE',
+//   useFactory: async ()=>{
+//     const DataSource = new DataSource({})
+//   }
+
+
+// }
+
+
+]
